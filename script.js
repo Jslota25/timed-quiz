04305b2e013349150811006636a223dbcaf838dd
John Slota <jslota17@comcast.net>
2020-03-02 20:54:38 -0600

    Created navbar

---
+++ script.js
@@ -0,0 +1,47 @@
+var questions = [
+    {
+        question: 'What is my middle name?',
+        potential_answers: [
+            {
+                id: 'a',
+                answer: 'Charles'
+            },
+            {
+                id: 'b',
+                answer: 'Frederick'
+            },
+            {
+                id: 'c',
+                answer: 'Allen'
+            },
+            {
+                id: 'd',
+                answer: "I don't have one."
+            }
+        ],
+        correct_answer: 'a'
+    },
+    {
+        question: 'Where did I first attend graduate school?',
+        potential_answers: [
+            {
+                id: 'a',
+                answer: 'The University of Utah'
+            },
+            {
+                id: 'b',
+                answer: 'Duke University'
+            },
+            {
+                id: 'c',
+                answer: 'Vanderbilt University'
+            },
+            {
+                id: 'd',
+                answer: 'The Penn State University'
+            }
+        ],
+        correct_answer: 'b'
+    }
+]
+
